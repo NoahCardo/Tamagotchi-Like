f680fc648336b0db3de16eb2759a2d44e2900ed1
Noah Cardozo <cardozonoah1505@outlook.com>
2025-06-20 04:00:48 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,19 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Bem-vindo ao Simulador de Animal de Estimação Virtual!");
-        System.out.println("Digite o nome do seu animal de estimação:");
+        System.out.println("Digite o Nome do seu Animal de Estimação:");
         String nomePet = scanner.nextLine();
 
         VirtualPet pet = new VirtualPet(nomePet);
 
         while (true) {
-            System.out.println("\nEscolha uma ação:");
+            System.out.println("\nEscolha uma Ação:");
             System.out.println("1. Alimentar " + nomePet);
             System.out.println("2. Brincar com " + nomePet);
-            System.out.println("3. Verificar o status de " + nomePet);
-            System.out.println("4. Sair");
+            System.out.println("3. Verificar o Status de " + nomePet);
+            System.out.println("4. Colocar " + nomePet + " para Dormir");
+            System.out.println("5. Cuidar da Higiene de " + nomePet);
+            System.out.println("6. Sair");
 
             int escolha = scanner.nextInt();
 
@@ -30,13 +32,29 @@ public class Main {
                     pet.verificarStatus();
                     break;
                 case 4:
+                    pet.descansar();
+                    break;
+                case 5:
+                    pet.higiene();
+                    break;
+                case 6:
                     System.out.println("Saindo do Simulador de Animal de Estimação Virtual. Adeus!");
                     return;
                 default:
-                    System.out.println("Escolha inválida. Tente novamente.");
+                    System.out.println("Escolha Inválida. Por Gentileza, Tente Outra Vez...");
             }
 
             pet.passarTempo(); // Simula o tempo que passa após cada ação
+
+            if (pet.verificarSaude()) {
+                System.out.println("Fim de Jogo!");
+                return;
+            }
+
+            if (pet.verificarVitoria()) {
+                System.out.println("Parabéns! Você Cuidou Muito Bem de Seu Pet e o Ajudou a Alcançar a Velhice Muito Feliz!~");
+                return;
+            }
         }
     }
 }
